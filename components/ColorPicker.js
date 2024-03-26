c581b3e078953f8139693c32cc9ac0070d8815b1
Niara M <niaraelyce@Niaras-MacBook-Air.local>
2024-03-26 18:22:11 -0400

    Attendance 3/26/24: useState

---
+++ components/ColorPicker.js
@@ -0,0 +1,19 @@
+const colors = [
+  {
+    name: "red",
+    value: "#ff0000",
+  },
+];
+
+const ColorPicker = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: colors[0].value,
+      }}
+    >
+      Color Picker
+    </div>
+  );
+};
+export default ColorPicker;
