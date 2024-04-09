947a6ddf199a6c597f6b9627bfe016fbec4273cd
Niara M <niaraelyce@Niaras-MacBook-Air.local>
2024-04-09 18:15:46 -0400

    Attendance 4/9/24: Days of the week

--- app/page.js
+++ app/page.js
@@ -1,40 +1,11 @@
 "use client";
+import { useState, useEffect } from "react";
 
 import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
 
 import { getPeople } from "../lib/api";
-
-const Homepage = () => {
-  const peopleArr = getPeople();
-  //console.log({ peopleArr });
-  return (
-    <div>
-      <h1>Weather app</h1>
-      <PeoplePicker people={peopleArr} />
-      <ButtonDemo />
-      <ColorPicker />
-    </div>
-  );
-};
-export default Homepage;
-import { getAllWeatherData } from "../lib/api";
-
-const Homepage = () => {
-  const data = getAllWeatherData();
-
-  return (
-    <div>
-      <h1>Weather App</h1>
-      <h2>{data.city.name}</h2>
-    </div>
-  );
-}
-export default Homepage;
-import { useState, useEffect } from "react";
-
-import { getAllWeatherData } from "../lib/api";
 
 const Homepage = () => {
   const [data, setData] = useState(null);
@@ -47,7 +18,12 @@ const Homepage = () => {
 
     fetchData();
   }, []);
-
+  useEffect(() => {
+    weatherData &&
+      weatherData.list.filter((block) => {
+        const date = new Date(block.dt);
+      });
+  }, [weatherData]);
   return (
     <div>
       <h1>Weather App</h1>
@@ -55,23 +31,21 @@ const Homepage = () => {
     </div>
   );
 };
+export default Homepage;
 // order the pizza
 export const getGeolocation = () => {
-  // handle whether the pizza is ready or not
-  // A promise expects two arguments: resolve and reject, which are functions that you call to fulfill or reject the promise.
   return new Promise((resolve, reject) => {
-    // if the pizza is ready, resolve the promise
-   if ("geolocation" in navigator) {
-    // Yes, the browser supports geolocation! Keep going!
-    // it needs two functions: success and error
-     navigator.geolocation.getCurrentPosition((position) => {
-        resolve(position);
-     }, () => {
-        reject("User denied geolocation");
-     });
-   } else {
-     // if the pizza is not ready, reject the promise
-     reject("Geolocation is not available in your browser");
-   }
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve(position);
+        },
+        () => {
+          reject("User denied geolocation");
+        }
+      );
+    } else {
+      reject("Geolocation is not available in your browser");
+    }
   });
 };
