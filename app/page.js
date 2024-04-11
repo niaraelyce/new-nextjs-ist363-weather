33c78b8698a0121c460926586cf2872c8d357ab4
Niara M <niaraelyce@Niaras-MacBook-Air.local>
2024-04-11 18:22:32 -0400

    Attendance 4/11/24: Conditional Styles

--- app/page.js
+++ app/page.js
@@ -1,51 +1,104 @@
 "use client";
+
 import { useState, useEffect } from "react";
+
+import Image from "next/image";
 
 import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
+import Tabs from "../components/Tabs";
 
-import { getPeople } from "../lib/api";
+import {
+  getGeoLocation,
+  getPeople,
+  getWeatherData,
+  getWeatherDataByLatLon,
+} from "../lib/api";
 
 const Homepage = () => {
-  const [data, setData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [daysOfWeek, setDaysOfWeek] = useState(null);
+  const [activeDayIndex, setActivedayIndex] = useState(0);
+
+  const peopleArr = getPeople();
+
+  useEffect(() => {
+    getGeoLocation()
+      .then((position) => {
+        console.log(position);
+        setLocation(position);
+      })
+      .catch((error) => {
+        setErrorMsg(error);
+      });
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getAllWeatherData();
-      setData(response);
+      const response = await getWeatherDataByLatLon(location);
+      setWeatherData(response);
     };
+    location ? fetchData() : null;
+  }, [location]);
 
-    fetchData();
-  }, []);
   useEffect(() => {
-    weatherData &&
-      weatherData.list.filter((block) => {
-        const date = new Date(block.dt);
+    const tempWeek = [];
+    if (weatherData) {
+      weatherData.list.forEach((block) => {
+        const date = new Date(block.dt * 1000);
+        const options = { weekday: "short" };
+        const day = date.toLocaleDateString("en-US", options);
+        if (!tempWeek.includes(day)) {
+          tempWeek.push(day);
+        }
       });
+      setDaysOfWeek(tempWeek);
+    }
   }, [weatherData]);
+
   return (
     <div>
-      <h1>Weather App</h1>
-      {data && <h2>{data.city.name}</h2>}
+      <h1>Weather app</h1>
+      {errorMsg && <div>{errorMsg}</div>}
+      {weatherData && (
+        <div>
+          <h2>{weatherData.city.name}</h2>
+          <p>Current temp: {weatherData.list[0].main.temp}&deg; F</p>
+          <p>{weatherData.list[0].weather[0].description}</p>
+          <Image
+            src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
+            alt={`Weather icon for ${weatherData.list[0].weather[0].description}`}
+            width={100}
+            height={100}
+          />
+        </div>
+      )}
+      {daysOfWeek && (
+        <section>
+          <Tabs
+            activeIndex={activeDayIndex}
+            items={daysOfWeek}
+            clickHandler={setActivedayIndex}
+          />
+          <div>
+            {weatherData?.list
+              .filter((block) => {
+                const date = new Date(block.dt * 1000);
+                const options = { weekday: "short" };
+                const day = date.toLocaleDateString("en-US", options);
+                return day === daysOfWeek[activeDayIndex];
+              })
+              .map((block, index) => {
+                return <p key={index}> {block.main.temp} </p>;
+              })}
+          </div>
+        </section>
+      )}
     </div>
   );
 };
+
 export default Homepage;
-// order the pizza
-export const getGeolocation = () => {
-  return new Promise((resolve, reject) => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve(position);
-        },
-        () => {
-          reject("User denied geolocation");
-        }
-      );
-    } else {
-      reject("Geolocation is not available in your browser");
-    }
-  });
-};
